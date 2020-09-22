434a3a04747d766c61459354cff307fe831872d5
CannedShroud <hypersync112@gmail.com>
2020-09-22 15:13:22 +0530

    Create Layout

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Card from "./Card";
+
+function Card() {
+  return (
+    <div className="card">
+      {/* Headline */}
+      {/* Title */}
+      {/* Description */}
+      {/* Action */}
+    </div>
+  );
+}
+
+export default Card;
---
+++ src/Cards.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Cards.css";
+
+function Cards() {
+  return (
+    <div className="cards">
+      {/* Card */}
+      {/* Card */}
+      {/* Card */}
+    </div>
+  );
+}
+
+export default Cards;
