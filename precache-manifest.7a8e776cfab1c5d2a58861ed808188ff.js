19c2ecf52691f24a1041c422ee81ea54b123fa90
CannedShroud <hypersync112@gmail.com>
2020-09-24 19:15:08 +0530

    Updates

---
+++ precache-manifest.7a8e776cfab1c5d2a58861ed808188ff.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ebe000e773af11ca89bc21fa9cd8519",
+    "url": "/dental-studio/index.html"
+  },
+  {
+    "revision": "6ce46f9aed4f56ccc2b7",
+    "url": "/dental-studio/static/css/main.7a10914d.chunk.css"
+  },
+  {
+    "revision": "b79f03c69c4b76f11cfe",
+    "url": "/dental-studio/static/js/2.8ec61449.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/dental-studio/static/js/2.8ec61449.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ce46f9aed4f56ccc2b7",
+    "url": "/dental-studio/static/js/main.4c889280.chunk.js"
+  },
+  {
+    "revision": "ad7b6d071ce6407344d3",
+    "url": "/dental-studio/static/js/runtime-main.f6888fa1.js"
+  },
+  {
+    "revision": "e451fdfc0fcd7fa92bc9d3d0a725c113",
+    "url": "/dental-studio/static/media/dentalChair.e451fdfc.jpg"
+  },
+  {
+    "revision": "931600189882542683985ff8ceacbb3e",
+    "url": "/dental-studio/static/media/logo.93160018.jpg"
+  }
+]);
